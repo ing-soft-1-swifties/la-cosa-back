317bddeb1bb1c3fdb4f3ba2f3077a03e4617cef9
wilitp <guilledeipola@gmail.com>
2023-10-02 12:57:29 -0300

    tests rooms service

---
+++ app/services/test_rooms.py
@@ -0,0 +1,12 @@
+from rooms import RoomsService
+import unittest
+
+class TestRoomsService(unittest.TestCase):
+    def test_join_user_successfully(self):
+        pass
+    def test_reject_duplicate_user(self):
+        pass
+    def test_reject_invalid_room(self):
+        pass
+
+
