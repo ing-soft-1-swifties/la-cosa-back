4da580fbbd0bd7db67c6fc0a7f6384d20e18ce76
Alejo_Corral <alejo.corral@mi.unc.edu.ar>
2023-10-16 18:34:15 -0300

    modifica servicio de checkeo de finalizacion de partida para que solo reciva un sid

--- app/services/games.py
+++ app/services/games.py
@@ -198,13 +198,19 @@ class GamesService(DBSessionMixin):
         return self.card_to_JSON(card_to_deal)
     
     @db_session
-    def end_game_condition(self, room:Room) -> str:
+    def end_game_condition(self, sent_sid : str) -> str:
         """Chequea si se finalizo la partida.
 
         Args: room (Room): current valid room
 
         Returns: str: {'GAME_IN_PROGRESS', 'LA_COSA_WON', 'HUMANS_WON'}
         """
+        
+        player = Player.get(sid = sent_sid)
+        #card = Card.get(id = payload["card_id"])
+        if player is None:
+            raise InvalidSidException()
+        room = player.playing
         
         ret = 'GAME_IN_PROGRESS'
         # Si queda solo un sobreviviente     
