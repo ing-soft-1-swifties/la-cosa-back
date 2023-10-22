660120c2e461f788d97eea0c13a5ad22f806b0ce
AlejoCorral2001 <AlejoCorral2001>
2023-10-22 03:56:02 -0300

    deja funcionando intercambio luego de descartar, genera tests semi automaticos para crear partidas, descartar e intercambiar cartas

--- app/sockets/test_sockets.py
+++ app/sockets/test_sockets.py
@@ -1,72 +1,179 @@
 #no son tests formales, se corren con python3 test_sockets.py
-#simula casos de usos simples, 
-
-def create_room(host_name, room_name):
-    import requests
-    url = 'http://localhost:8000/create'
-    data={
-    "room_name": room_name,
-    "host_name": host_name,
-    "min_players": 4,
-    "max_players": 5,
-    "is_private": False
-    }
-    response = requests.post(url, json=data)
-    token = None
-    if response.status_code == 200:
-        print('Solicitud exitosa')
-        print('Respuesta del servidor:', response.json())
-        token = response.json().get("token")
-    else:
-        print('La solicitud falló con el código de estado:', response.status_code)
-        print('Respuesta del servidor:', response.text)
-    return token
-
-def join_room(player_name, room_id):
-    import requests
-    url = 'http://localhost:8000/join'
-    data={
-    "room_id": room_id,
-    "name": player_name,
-    }
-    response = requests.post(url, json=data)
-    token = None
-    if response.status_code == 200:
-        print('Solicitud exitosa')
-        print('Respuesta del servidor:', response.json())
-        token = response.json().get("token")
-    else:
-        print('La solicitud falló con el código de estado:', response.status_code)
-        print('Respuesta del servidor:', response.text)
-    return token
-
+#simula casos de usos simples, para este caso simula una creacion y lanzamiento de una partida
+#seguido de un descarte y un intercambio exitoso de cartas. No se realizan checkeos automaticos por ahora 
 
 import socketio
-def connect_socket(token):
-    import requests
-    sio = socketio.Client()
-
-    sio.connect('http://localhost:8000', auth={"token":token})
-    # Espera a que la conexión se establezca
-    print(f"conexion establecioda para token {token}")
-    # Simula el envío de un mensaje al servidor
-    # sio.emit('message', 'Hola desde el cliente')
-    return sio
-
-def listen_events(sio):
-    def on_any_event(sid, data):
-        print(data)
-    sio.on("*", on_any_event) 
-    sio.wait()
-
 import threading
+import time
 
-host_token = create_room("111", "test")
-players_sockets = []
-players_sockets.append(connect_socket(host_token))
-for i in range(2,5):
-    player_token = join_room(f"{i}{i}{i}", 1)
-    players_sockets.append(connect_socket(player_token))
-listening_thread = threading.Thread(target=listen_events, args=(players_sockets[0],))
-listening_thread.start()
-players_sockets[0].emit("room_start_game") 
+class player():
+    def __init__(self, name, room_id):
+        self.name = name
+        self.room_id = room_id
+        self.token = self.join_room()
+        self.sio = self.connect_socket()
+        self.position = 0
+        self.cards = []
+        threading.Thread(target=self.listen_events).start()
+    def join_room(self):
+        import requests
+        url = 'http://localhost:8000/join'
+        data={
+        "room_id": self.room_id,
+        "name": self.name,
+        }
+        response = requests.post(url, json=data)
+        token = None
+        if response.status_code == 200:
+            # print('Solicitud exitosa')
+            token = response.json().get("token")
+        else:
+            print('La solicitud falló con el código de estado:', response.status_code)
+            print('Respuesta del servidor:', response.text)
+            quit()
+        return token
+    def connect_socket(self):
+        sio = socketio.Client()
+        sio.connect('http://localhost:8000', auth={"token":self.token})
+        # print(f"conexion establecioda para token {self.token}")
+        return sio
+    def listen_events(self):
+        @self.sio.on('*')
+        def on_any_event(event, data):
+            self.cards = []
+            for carta in data["gameState"]["playerData"]["cards"]:
+                self.cards.append({"id":carta["id"],
+                                   "name":carta["name"]})
+            self.position = data["gameState"]["playerData"]["position"]
+
+        self.sio.wait()
+    def emit(self, event, body):
+        if body == {}:
+            self.sio.emit(event)
+        else:
+            self.sio.emit(event, body)
+    def discard_random_card(self):
+        for card in self.cards:
+            if card["name"] != "La cosa" \
+            or card["name"] != "Infección":
+                self.emit("game_discard_card",{"card":card["id"]})
+                return
+    def exchange_random_card(self):
+        for card in self.cards:
+            if card["name"] != "La cosa" \
+            or card["name"] != "Infección":
+                self.emit("game_exchange_card",{"card":card["id"], "on_defense":False, "target":0})
+                return
+    def show_cards(self):
+        print(self.cards)
+class host(player):
+    def __init__(self, name, room_name):
+        self.room_name = room_name
+        self.name = name
+        self.position = 0
+        self.cards = []
+        self.token = self.create_room("test")
+        self.sio = self.connect_socket()
+        threading.Thread(target=self.listen_events).start()
+    def create_room(self,room_name):
+        import requests
+        url = 'http://localhost:8000/create'
+        data={
+        "room_name": room_name,
+        "host_name": self.name,
+        "min_players": 4,
+        "max_players": 5,
+        "is_private": False
+        }
+        response = requests.post(url, json=data)
+        token = None
+        if response.status_code == 200:
+            # print('Solicitud exitosa')
+            # print('Respuesta del servidor:', response.json())
+            token = response.json().get("token")
+        else:
+            print('La solicitud falló con el código de estado:', response.status_code)
+            print('Respuesta del servidor:', response.text)
+        return token
+    def listen_events(self):
+        @self.sio.on('*')
+        def on_any_event(event, data):
+            self.cards = []
+            for carta in data["gameState"]["playerData"]["cards"]:
+                self.cards.append({"id":carta["id"],
+                                   "name":carta["name"]})
+            self.position = data["gameState"]["playerData"]["position"]
+            # print(" ")
+            # print("Host escucho el siguiente evento: ", event)
+            # print(" ")
+            # import json
+            # print(json.dumps(data, indent=2))
+            # print(f"jugador {self.name} tiene estas cartas: ", data["gameState"]["playerData"]["cards"])
+
+class game():
+    def __init__(self):
+        self.players = []
+        self.hoster = host("111", f"test")
+        print("Partida creada con exito")
+        print("Jugador 1 se unio correctamente")
+        self.room_id = self.last_room_id()
+        self.players.append(self.hoster)
+        for i in range(2,5):
+            self.players.append(player(f"{i}{i}{i}", self.room_id))
+            print(f"Jugador {i} se unio correctamente")
+        # self.start()
+        # print("Partida iniciada con exito")
+    def start(self):
+        self.hoster.emit("room_start_game", {})
+    def last_room_id(self):
+        import requests
+        url = 'http://localhost:8000/list'
+        response = requests.get(url)
+        if response.status_code == 200:
+            # print('Solicitud exitosa')
+            # print('Respuesta del servidor:', response.json())
+            return int(list(response.json())[-1]["id"])
+            # token = response.json().get("token")
+        else:
+            print('La solicitud falló con el código de estado:', response.status_code)
+            print('Respuesta del servidor:', response.text)
+            quit()
+    def player_in_turn(self):
+        for i, player in enumerate(self.players):
+            if len(player.cards) == 5:
+                return player
+    def discard_card(self):
+        self.player_in_turn = self.player_in_turn()
+        self.player_in_turn.discard_random_card()
+    def exchange_card(self):
+        self.player_in_turn.exchange_random_card()
+        time.sleep(1)
+        next_player = None
+        for player in self.players:
+            #notar que no anda para el caso borde de el ultimo jugador
+            if player.position == self.player_in_turn.position+1:
+                next_player = player
+                break
+        print("cartas de el primer jugador antes del intercambio: ")
+        self.player_in_turn.show_cards()
+        print("cartas de el segundo jugador antes del intercambio: ")
+        next_player.show_cards()
+        next_player.exchange_random_card()
+        time.sleep(1)
+        print("luego del intercambio: ")
+        print("cartas de el primer jugador: ")
+        self.player_in_turn.show_cards()
+        print("cartas de el segundo jugador: ")
+        next_player.show_cards()
+        
+    
+
+current_game = game()
+current_game.start()
+print("se inició la partida")
+time.sleep(1)
+current_game.discard_card()
+print("se descarto una carta")
+time.sleep(1)
+current_game.exchange_card()
+print("se intercambiaron las cartas")
