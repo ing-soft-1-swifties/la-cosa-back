e4bc8763994ecdc6e033fb06ec84dcdeda8e3189
Ebner Lautaro <lautaro.ebner@mi.unc.edu.ar>
2023-11-12 11:10:49 -0300

    tests agregados

--- app/services/test_play_card.py
+++ app/services/test_play_card.py
@@ -420,12 +420,40 @@ class TestPlayCardsService(unittest.TestCase):
                 card_options={'target': 2131231231}  # INVALID
             )
 
+    @db_session
+    def test_play_vigila_tus_espaldas(self):
+        TEST_NAME = 'test_play_vigila_tus_espaldas'
+        # creamos una room valida
+        room = self.create_valid_room(roomname=TEST_NAME, qty_players=12)
 
+        # seleccionamos un jugador al azar y le damos la carta
+        player: Player = room.players.select(lambda p: p.position == 0).first()
+        room.turn = player.position
+        card = Card.select(lambda c: c.name == 'Vigila tus espaldas').first()
+        player.add_card(card.id)
 
+        # guardamos la direccion vieja
+        old_direction = room.direction
 
+        response = self.pcs.play_vigila_tus_espaldas(
+            player=player,
+            room=room,
+            card=card,
+            card_options={}  # INTVALID
+        )
 
+        # comportamiento esperado
+        assert room.direction != old_direction
 
+        assert len(response) == 1
 
+        print(response)
+
+        assert response[0]['name'] == 'on_game_player_play_card'
+        assert response[0]['body']['card_id'] == card.id
+        assert response[0]['body']['card_name'] == card.name
+        assert response[0]['body']['player_name'] == player.name
+        assert response[0]['broadcast']
 
     @classmethod
     @db_session
